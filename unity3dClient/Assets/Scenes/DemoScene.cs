8ff7db8c169c3db5710df95805cb429ef43dd453
HAIJIANG JIN <hjj2021@outlook.com>
2022-11-16 22:17:45 +0800

    完善代码逻辑；

--- unity3dClient/Assets/Scenes/DemoScene.cs
+++ unity3dClient/Assets/Scenes/DemoScene.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+
 using Spine;
 using Spine.Unity;
 using Spine.Unity.AttachmentTools; // XXX 注意: 这里有个引用
@@ -6,8 +8,14 @@ using UnityEngine;
 
 public class DemoScene : MonoBehaviour
 {
-    void Start()
+    /// <summary>
+    /// Start
+    /// </summary>
+    /// <returns>枚举迭代器</returns>
+    IEnumerator Start()
     {
+        yield return 1;
+
         // 只加载 Spine 的合图资源,
         // 武器没有导入骨骼动画
         SpineAtlasAsset spAtlasAsset = Resources.Load<SpineAtlasAsset>("Weaponz/Spine/Weapon_1xxx__Atlas");
@@ -25,5 +33,45 @@ public class DemoScene : MonoBehaviour
         SkeletonAnimation spAnim = GameObject.Find("/Character_1002_").GetComponent<SkeletonAnimation>();
         // 查找右手武器插槽, 并替换其中的附件
         spAnim.Skeleton.FindSlot("Slot_RWeapon").Attachment = rWeapon;
+
+        // 等待 2 秒
+        yield return new WaitForSeconds(2);
+
+        // 加载武器图片
+        Sprite spriteWeapon = Resources.Load<Sprite>("Weaponz/Weapon_1002_");
+
+        Material materialTemp = new Material(Shader.Find("Spine/Skeleton"));
+        materialTemp.mainTexture = spriteWeapon.texture;
+        rWeapon = spriteWeapon.ToRegionAttachment(materialTemp);
+
+        spAnim.Skeleton.FindSlot("Slot_RWeapon").Attachment = rWeapon;
+
+        yield return new WaitForSeconds(2);
+
+        Skin weaponeSkin = new Skin("NewWeapon");
+        weaponeSkin.SetAttachment(2048, "X", rWeapon);
+
+        Skin defaultSkin = spAnim.Skeleton.Data.FindSkin("default");
+
+        Skin newSkin = new Skin("Mixed");
+        newSkin.AddSkin(weaponeSkin);
+        newSkin.AddSkin(defaultSkin);
+
+        // 注意: 这里是一步优化操作, 将用到的图片打包到一个新的图片合集中.
+        // 这样可以降低 DrawCall 次数
+        Spine.Skin repackedSkin = newSkin.GetRepackedSkin(
+            "RepackedSkin",
+            spAnim.SkeletonDataAsset.atlasAssets[0].PrimaryMaterial,
+            out var _,
+            out var _
+        );
+
+        spAnim.Skeleton.SetSkin(repackedSkin);
+        spAnim.skeleton.UpdateCache();
+        spAnim.skeleton.SetSlotsToSetupPose();
+
+        yield return new WaitForSeconds(2);
+
+        spAnim.Skeleton.FindSlot("Slot_RWeapon").Attachment = repackedSkin.GetAttachment(2048, "X");
     }
 }
