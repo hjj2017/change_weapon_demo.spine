0e59d5512493c927d31b6e987314650d3c0f59c5
HAIJIANG JIN <hjj2021@outlook.com>
2022-11-17 11:36:24 +0800

    完善换武器演示代码；

--- unity3dClient/Assets/Scenes/DemoScene.cs
+++ unity3dClient/Assets/Scenes/DemoScene.cs
@@ -16,65 +16,65 @@ public class DemoScene : MonoBehaviour
     {
         yield return 1;
 
-        //// 只加载 Spine 的合图资源,
-        //// 武器没有导入骨骼动画
-        //SpineAtlasAsset spAtlasAsset = Resources.Load<SpineAtlasAsset>("Weaponz/Spine/Weapon_1xxx__Atlas");
-        //Atlas spAtlas = spAtlasAsset.GetAtlas();
+        // 只加载 Spine 的合图资源,
+        // 武器没有导入骨骼动画
+        SpineAtlasAsset spAtlasAsset = Resources.Load<SpineAtlasAsset>("Weaponz/Spine/Weapon_1xxx__Atlas");
+        Atlas spAtlas = spAtlasAsset.GetAtlas();
 
-        //// 查找编号 = 1001 这个武器在合图中的区域位置
-        //AtlasRegion atlasReg = spAtlas.FindRegion("img/Weapon_1001_");
+        // 查找编号 = 1001 这个武器在合图中的区域位置
+        AtlasRegion atlasReg = spAtlas.FindRegion("img/Weapon_1001_");
 
-        //// 创建附件,
-        //// XXX 注意: 要成功调用 ToRegionAttachment 函数,
-        //// 必须先引用 Spine.Unity.AttachmentTools
-        //RegionAttachment rWeapon = atlasReg.ToRegionAttachment(atlasReg.name);
+        // 创建附件,
+        // XXX 注意: 要成功调用 ToRegionAttachment 函数,
+        // 必须先引用 Spine.Unity.AttachmentTools
+        RegionAttachment rWeapon = atlasReg.ToRegionAttachment(atlasReg.name);
 
-        //// 获取主角的 Spine 骨骼动画
-        //SkeletonAnimation spAnim = GameObject.Find("/Character_X_").GetComponent<SkeletonAnimation>();
-        //// 查找右手武器插槽, 并替换其中的附件
-        //spAnim.Skeleton.FindSlot("Slot_RWeapon").Attachment = rWeapon;
+        // 获取主角的 Spine 骨骼动画
+        SkeletonAnimation spAnim = GameObject.Find("/Character_X_").GetComponent<SkeletonAnimation>();
+        // 查找右手武器插槽, 并替换其中的附件
+        spAnim.Skeleton.FindSlot("Slot_RWeapon").Attachment = rWeapon;
 
-        //// 等待 2 秒
-        //yield return new WaitForSeconds(2);
+        // 等待 2 秒
+        yield return new WaitForSeconds(2);
 
-        //// 加载武器图片
-        //Sprite spriteWeapon = Resources.Load<Sprite>("Weaponz/Weapon_1002_");
+        // 加载武器图片
+        Sprite spriteWeapon = Resources.Load<Sprite>("Weaponz/Weapon_1002_");
 
-        //Material materialTemp = new Material(Shader.Find("Spine/Skeleton"));
-        //materialTemp.mainTexture = spriteWeapon.texture;
-        //rWeapon = spriteWeapon.ToRegionAttachment(materialTemp);
+        Material materialTemp = new Material(Shader.Find("Spine/Skeleton"));
+        materialTemp.mainTexture = spriteWeapon.texture;
+        rWeapon = spriteWeapon.ToRegionAttachment(materialTemp);
 
-        //spAnim.Skeleton.FindSlot("Slot_RWeapon").Attachment = rWeapon;
+        spAnim.Skeleton.FindSlot("Slot_RWeapon").Attachment = rWeapon;
 
-        //yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(2);
 
-        //Skin weaponeSkin = new Skin("NewWeapon");
-        //weaponeSkin.SetAttachment(2048, "X", rWeapon);
+        Skin weaponeSkin = new Skin("NewWeapon");
+        weaponeSkin.SetAttachment(1, "X", rWeapon);
 
-        //Skin defaultSkin = spAnim.Skeleton.Data.FindSkin("_Default");
+        Skin defaultSkin = spAnim.Skeleton.Data.FindSkin("_Default");
 
-        //Skin newSkin = new Skin("Mixed");
-        //newSkin.AddSkin(weaponeSkin);
-        //newSkin.AddSkin(defaultSkin);
+        Skin newSkin = new Skin("Mixed");
+        newSkin.AddSkin(defaultSkin);
+        newSkin.AddSkin(weaponeSkin);
 
-        //// 注意: 这里是一步优化操作, 将用到的图片打包到一个新的图片合集中.
-        //// 这样可以降低 DrawCall 次数
-        //Spine.Skin repackedSkin = newSkin.GetRepackedSkin(
-        //    "RepackedSkin",
-        //    spAnim.SkeletonDataAsset.atlasAssets[0].PrimaryMaterial,
-        //    out var _,
-        //    out var _
-        //);
+        // 注意: 这里是一步优化操作, 将用到的图片打包到一个新的图片合集中.
+        // 这样可以降低 DrawCall 次数
+        Spine.Skin repackedSkin = newSkin.GetRepackedSkin(
+            "RepackedSkin",
+            spAnim.SkeletonDataAsset.atlasAssets[0].PrimaryMaterial,
+            out var _,
+            out var _
+        );
 
-        //spAnim.Skeleton.SetSkin(repackedSkin);
-        //spAnim.skeleton.UpdateCache();
-        //spAnim.skeleton.SetSlotsToSetupPose();
+        spAnim.Skeleton.SetSkin(repackedSkin);
+        spAnim.skeleton.UpdateCache();
+        spAnim.skeleton.SetSlotsToSetupPose();
 
-        //yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(2);
 
-        //spAnim.Skeleton.FindSlot("Slot_RWeapon").Attachment = repackedSkin.GetAttachment(2048, "X");
+        spAnim.Skeleton.FindSlot("Slot_RWeapon").Attachment = repackedSkin.GetAttachment(1, "X");
 
-        //yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(2);
 
         ReplaceBothLeg();
     }
@@ -111,7 +111,7 @@ public class DemoScene : MonoBehaviour
         skinMixed.AddSkin(skinDefault);
 
         Spine.Skin skinRepacked = skinMixed.GetRepackedSkin(
-            "RepackedSkin",
+            "RepackedSkin2",
             spAnim.SkeletonDataAsset.atlasAssets[0].PrimaryMaterial,
             out var _,
             out var _
